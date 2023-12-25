364d90a81a484702f9101587ab279bbdb1df29d5
Tomás Saavedra <tomas.saavedra@sociuschile.cl>
2023-12-24 22:47:37 -0300

    Html and Script added

---
+++ 2_arrow.js
@@ -0,0 +1,5 @@
+// const suma = function(a,b){
+//     return a+b
+// }
+
+const suma = (a,b) => a + b
---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const ele = document.getElementById("ele1")
+const pintar = function(color = 'green'){
+    
+    ele.style.backgroundColor = 'yellow'
+}
+ele.addEventListener("click", pintar)
+
